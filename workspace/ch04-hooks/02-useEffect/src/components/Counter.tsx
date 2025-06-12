09b6723dd435f46c617dc477adcc5e642f66934f
seonyoungg <tjsduddlfma1@naver.com>
2025-06-12 19:59:39 +0900

    0612 리액트수업

--- workspace/ch04-hooks/02-useEffect/src/components/Counter.tsx
+++ workspace/ch04-hooks/02-useEffect/src/components/Counter.tsx
@@ -13,8 +13,6 @@ function Counter({ children = '1' }: CounterProps) {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(initCount);
 
-  // TODO 2. 증감값을 입력하면 입력값만큼 증감
-
   // TODO 1. 1초 후에 handleUp()을 호출해서 자동으로 값 한번 증가
 
   // useEffect(이팩트 함수, []) :: mount, unmount시 한번만 실행
@@ -22,34 +20,65 @@ function Counter({ children = '1' }: CounterProps) {
 
   // useEffect(이팩트함수) :: 컴포넌트가 업데이트 될 때마다 실행
 
+  // 마운트 된 이후에 실행
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log('dependencies에 빈배열 지정 시 마운트 된 후에 한번만 호출');
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log('마운트 됬찌롱');
+  //   return () => {
+  //     console.log('언마운트 됬찌롱');
+  //   };
+  // }, []);
+
   useEffect(() => {
-    setTimeout(() => {
-      handleUp();
+    // setup
+    console.log('setup 함수  호출');
+    const timer = setInterval(() => {
+      console.log(new Date());
     }, 1000);
 
-    console.log('dependencies에 빈배열 지정 시 마운트 된 후에 한번만 호출');
-  }, []);
+    // setup 함수의 return => cleanup
+    // 1. 컴포넌트가 언마운트될 때 호출
+    // 2. setup  함수가 재실행 되기 전에 호출
+    return () => {
+      console.log('cleanup 함수 호출');
+      clearInterval(timer);
+    };
+  });
+
+  console.log('렌더링 중', document.querySelector('span')?.textContent);
 
   useEffect(() => {
-    console.log('마운트 됬찌롱');
-    return () => {
-      console.log('언마운트 됬찌롱');
-    };
-  }, []);
+    console.log('렌더링 후', document.querySelector('span')?.textContent);
+  });
+
   // 카운터 감소
   const handleDown = () => {
-    setCount(count - 1);
+    setCount(count - step);
   };
 
   // 카운터 증가
   const handleUp = () => {
-    setCount(count + 1);
+    setCount(count + step);
   };
 
   // 카운터 초기화
   const handleReset = () => {
     setCount(0);
   };
+
+  // TODO 2. 증감치가 수정되면 증감치만큼 1회 자동 증가  useEffect(() => {
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     handleUp();
+  //   }, 1000);
+  //   console.log('dependecies에 값을 지정하면 컴포넌트가 업데이트 될 때 지정한 값 중 하나라도 수정되었을 경우 호출됨 ');
+  // }, [step]);
 
   return (
     <div id='counter'>
