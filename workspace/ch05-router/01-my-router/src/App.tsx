ac43d252036b3de792efb4d739bd9c66f28c64f9
seonyoungg <tjsduddlfma1@naver.com>
2025-06-17 14:22:11 +0900

    0617 리액트수업(2)

--- workspace/ch05-router/01-my-router/src/App.tsx
+++ workspace/ch05-router/01-my-router/src/App.tsx
@@ -1,11 +1,38 @@
-import Home from "@pages/Home";
+import Home from '@pages/Home';
+import Page1 from '@pages/Page1';
+import Page2 from '@pages/Page2';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );  
+  // 현재 URL을 상태로 관리
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+
+  // URL이 변경되면 컴포넌트를 교체한다.
+  const handleLocationChange = () => {
+    console.log(location.pathname, '주소변경됨');
+
+    // APP이 리렌더링 되어야 한다
+    setCurrentPath(location.pathname);
+  };
+
+  // popstate 이벤트가 발생하면 handleLocationChange 호출
+  window.addEventListener('popstate', handleLocationChange);
+
+  // url에 맞는 컴포넌트를 반환
+  const renderComponent = () => {
+    switch (currentPath) {
+      case '/':
+      case '/home':
+        return <Home />;
+      case '/page1':
+        return <Page1 />;
+      case '/page2':
+        return <Page2 />;
+      default:
+        return <p>404 에러</p>;
+    }
+  };
+  return <>{renderComponent()}</>;
 }
 
-export default App
+export default App;
--- workspace/ch05-router/01-my-router/src/components/Header.tsx
+++ workspace/ch05-router/01-my-router/src/components/Header.tsx
@@ -4,11 +4,11 @@ function Header() {
   return (
     <header>
       <h1>리액트 라우터 - 01 클라이언트 라우팅 직접 구현</h1>
-      <Mylink to='home.html'>home</Mylink>
+      <Mylink to='/home'>home</Mylink>
       <br />
-      <Mylink to='page1.html'>page1</Mylink>
+      <Mylink to='/page1'>page1</Mylink>
       <br />
-      <Mylink to='page2.html'>page2</Mylink>
+      <Mylink to='/page2'>page2</Mylink>
     </header>
   );
 }
