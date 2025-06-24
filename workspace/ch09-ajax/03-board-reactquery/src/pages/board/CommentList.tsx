c413a50ab71c71a14289ea8ec93022da2b1457ea
seonyoungg <tjsduddlfma1@naver.com>
2025-06-24 17:18:38 +0900

    6/24 리액트마무리

--- workspace/ch09-ajax/03-board-reactquery/src/pages/board/CommentList.tsx
+++ workspace/ch09-ajax/03-board-reactquery/src/pages/board/CommentList.tsx
@@ -9,19 +9,32 @@ function CommentList() {
 
   const { data, isLoading, error } = useQuery({
     queryKey: ['posts', 1, 'replies'],
-    queryFn: () => axios.get('posts/1/replies?delay=1000'),
+    queryFn: () =>
+      axios.get('posts/1/replies?delay=1000', {
+        params: {
+          // delay: Math.random() * 6000,
+          delay: 1000,
+        },
+      }),
     // queryFn: async () => await axios.get('posts/1?delay=1000'),
     select: (response: { data: ReplyListResType }) => response.data.item,
 
+    // TODO 작업이 실패하면 자동으로 재시도하기(catch 블럭에서 지정한 횟수만큼 requestCommentList() 호출 => 에러 시 무한반복 방지)
+    // TODO 다른탭이나 앱에서 작업 후에 돌아오면 데이터 자동으로 갱신하기
+    //      - document visibilitychange : 브라우저의 가시성 변경을 감지
+    //      - window에 focus 이벤트로 브라우저 탭의 포커스  변경 감지
+    //      - requestCommentList() 호출)
+    retry: 3, // 작업이 실패하면 자동으로 재시도하기(default = 3)
+    refetchOnWindowFocus: true, //(기본이 true)
+
     /**
-      // TODO 작업이 실패하면 자동으로 재시도하기(catch 블럭에서 지정한 횟수만큼 requestCommentList() 호출 => 에러 시 무한반복 방지)
-      // TODO 다른탭이나 앱에서 작업 후에 돌아오면 데이터 자동으로 갱신하기
-      //      - document visibilitychange : 브라우저의 가시성 변경을 감지
-      //      - window에 focus 이벤트로 브라우저 탭의 포커스  변경 감지
-      //      - requestCommentList() 호출)
-      // TODO 일정시간동안은 캐시해서 서버 호출 횟수 줄이기
-      // TODO 주기적으로 호출해서 데이터를 자동으로 갱신하기 setInterval() 
-    // */
+    staleTime: 1000 * 20,
+    refetchInterval: 1000 * 5,
+    5초에 한번씩 20초간 캐싱된걸 로드
+     */
+
+    staleTime: 1000 * 5, // TODO 일정시간동안은 캐시해서 서버 호출 횟수 줄이기
+    refetchInterval: 1000 * 3, // TODO 주기적으로 호출해서 데이터를 자동으로 갱신하기 setInterval()
   });
 
   const replyList = data?.map((reply) => <li key={reply._id}>{reply.content}</li>);
--- workspace/ch09-ajax/03-board-reactquery/src/pages/board/CommentNew.tsx
+++ workspace/ch09-ajax/03-board-reactquery/src/pages/board/CommentNew.tsx
@@ -1,4 +1,5 @@
 import useAxiosInstance from '@/hooks/useAxiosInstance';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 
 function CommentNew() {
@@ -8,23 +9,23 @@ function CommentNew() {
   // axios instance
   const axios = useAxiosInstance();
 
-  // API 서버에 댓글 등록 요청
-  const requestAddComment = async (formData: FormData) => {
-    try {
-      const response = await axios.post('/posts/1/replies', formData);
-    } catch (err) {
-      // TODO 댓글 등록 후 댓글 목록 갱신(requestCommentList()를 props로 전달받아서 호출)
-      // alert('게시물 상세 조회에 실패했습니다.\n잠시 후 다시 요청하시기 바랍니다.');
-      console.error(err);
-    }
-  };
+  const queryClient = useQueryClient();
+  const { mutate, isPending } = useMutation({
+    mutationFn: (formData: FormData) => axios.post('/posts/1/replies', formData),
+    onSuccess() {
+      //TODO 댓글 등록 후 댓글 목록 갱신(requestCommentList()를 props로 전달받아서 호출)
+
+      queryClient.invalidateQueries({ queryKey: ['posts', 1, 'replies'] });
+      setContent('');
+    },
+  });
 
   const handleAddComment = (event: React.FormEvent<HTMLFormElement>) => {
     // TODO 중복 요청 방지(요청 시작 전에 버튼 비활성화, 응답 완료 후에 버튼 활성화)
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
-    requestAddComment(formData);
+    mutate(formData);
   };
 
   return (
@@ -33,7 +34,9 @@ function CommentNew() {
       <form onSubmit={handleAddComment}>
         <textarea name='content' rows={3} cols={30} placeholder='댓글 내용' value={content} onChange={(e) => setContent(e.target.value)} />
         <br />
-        <button type='submit'>등록</button>
+        <button type='submit' disabled={isPending}>
+          등록
+        </button>
       </form>
     </>
   );
