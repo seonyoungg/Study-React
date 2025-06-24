387c6512b764cc7ed83a26915b960aa216a4d374
seonyoungg <tjsduddlfma1@naver.com>
2025-06-24 11:23:36 +0900

    0624 리액트수업(4)

--- workspace/ch09-ajax/02-board-axios/src/pages/board/BoardInfo.tsx
+++ workspace/ch09-ajax/02-board-axios/src/pages/board/BoardInfo.tsx
@@ -1,6 +1,6 @@
 import useAxiosInstance from '@/hooks/useAxiosInstance';
 import CommentList from '@/pages/board/CommentList';
-import type { BoardInfoType, ReplyListResType } from '@/types/BoardType';
+import type { BoardInfoResType, BoardInfoType } from '@/types/BoardType';
 import { useEffect, useState } from 'react';
 
 function BoardInfo() {
@@ -13,39 +13,39 @@ function BoardInfo() {
   // 에러 상태
   const [error, setError] = useState<Error | null>(null);
 
-  //axios instance
+  // axios instance
   const axios = useAxiosInstance();
 
-  // API 서버에 1번 게시물의 상세 정보를 fetch() 요청으로 보낸다
+  // API 서버에 1번 게시물의 상세정보를 fetch() 요청으로 보낸다.
   const requestInfo = async () => {
     try {
-      // 로딩상태 true 지정
+      // 로딩 상태를 true로 지정
       setIsLoading(true);
 
-      const response = await axios.get<ReplyListResType>('/posts/1', {
-        params: {
-          delay: 1000,
-        },
-      });
+      const response = await axios.get<BoardInfoResType>('/posts/1?delay=1000');
+
+      // 게시물 상세 정보 출력
       setData(response.data.item);
+      setError(null);
     } catch (err) {
       setError(err as Error);
       setData(null);
       console.error(err);
     } finally {
+      // 성공, 실패와 상관 없이 로딩 상태를 false로 지정
       setIsLoading(false);
     }
   };
 
   useEffect(() => {
     requestInfo();
-  }, []);
+  }, []); // 마운트 후에 한번만 실행
 
   return (
     <>
       <h1>02 Axios 라이브러리</h1>
 
-      {isLoading && <p>로딩중 ...</p>}
+      {isLoading && <p>로딩중...</p>}
       {error && <p>{error.message}</p>}
       {data && (
         <>
--- workspace/ch09-ajax/02-board-axios/src/pages/board/CommentList.tsx
+++ workspace/ch09-ajax/02-board-axios/src/pages/board/CommentList.tsx
@@ -1,16 +1,11 @@
 import useAxiosInstance from '@/hooks/useAxiosInstance';
 import CommentNew from '@/pages/board/CommentNew';
-import type { BoardInfoResType, BoardInfoType, ReplyListResType } from '@/types/BoardType';
+import type { ReplyListResType, ReplyType } from '@/types/BoardType';
 import { useEffect, useState } from 'react';
 
-// interface PropType {
-//   replies: ReplyType[];
-// }
-
-// function CommentList({ replies = [] }: PropType) {
 function CommentList() {
   // 서버의 데이터를 저장할 상태
-  const [data, setData] = useState<BoardInfoType | null>(null);
+  const [data, setData] = useState<ReplyType[] | null>(null);
 
   // 로딩 상태
   const [isLoading, setIsLoading] = useState(false);
@@ -18,13 +13,13 @@ function CommentList() {
   // 에러 상태
   const [error, setError] = useState<Error | null>(null);
 
-  //axios instance
+  // axios instance
   const axios = useAxiosInstance();
 
-  // API 서버에 1번 게시물의 댓글 목록을 fetch() 요청으로 보낸다
+  // API 서버에 1번 게시물의 댓글 목록을 fetch() 요청으로 보낸다.
   const requestCommentList = async () => {
     try {
-      // 로딩상태 true 지정
+      // 로딩 상태를 true로 지정
       setIsLoading(true);
 
       const response = await axios.get<ReplyListResType>('/posts/1/replies', {
@@ -34,35 +29,45 @@ function CommentList() {
           limit: 10,
         },
       });
+
+      // 댓글 목록 출력
       setData(response.data.item);
+      setError(null);
     } catch (err) {
       setError(err as Error);
       setData(null);
       console.error(err);
     } finally {
+      // 성공, 실패와 상관 없이 로딩 상태를 false로 지정
       setIsLoading(false);
     }
   };
 
   useEffect(() => {
     requestCommentList();
-  }, []);
+  }, []); // 마운트 후에 한번만 실행
 
-  // const replyList = replies.map((reply) => <li key={reply._id}>{reply.content}</li>);
   const replyList = data?.map((reply) => <li key={reply._id}>{reply.content}</li>);
+
+  let content = null;
+  if (isLoading) {
+    content = <p>댓글 로딩중...</p>;
+  } else if (error) {
+    content = <p>{error.message}</p>;
+  } else if (data) {
+    content = (
+      <>
+        <ul>{replyList}</ul>
+        <CommentNew />
+      </>
+    );
+  }
+
   return (
     <>
       <h3>댓글 목록</h3>
 
-      {isLoading && <p>댓글 로딩중 ...</p>}
-      {error && <p>{error.message}</p>}
-
-      {data && (
-        <>
-          <ul>{replyList}</ul>
-          <CommentNew />
-        </>
-      )}
+      {content}
     </>
   );
 }
