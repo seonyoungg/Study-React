190f6bb7e068d2c042fc8f31e2f9c2a00f9474a8
seonyoungg <tjsduddlfma1@naver.com>
2025-06-25 20:27:29 +0900

    6/25 리액트(4)

--- workspace/ch07-globalstate/01-redux/src/components/Left3.tsx
+++ workspace/ch07-globalstate/01-redux/src/components/Left3.tsx
@@ -1,3 +1,4 @@
+import type { RootState } from '@/RTK/store';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
@@ -6,8 +7,12 @@ function Left3() {
     console.log('#### Left3 렌더링.');
   });
 
-  // Redux store에서 count값 가져오기
-  const count = useSelector((state: { count: number }) => state.count);
+  // Redux store에서 count 값 가져오기
+  // const count = useSelector((state: {count: number}) => state.count);
+
+  // RTK에서 store에서 count값 가져오기
+  const count = useSelector((state: RootState) => state.counterStore.count);
+
   return (
     <div>
       <h3>Left3</h3>
--- workspace/ch07-globalstate/01-redux/src/main.tsx
+++ workspace/ch07-globalstate/01-redux/src/main.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
 
-import store from '@redux/store';
+// Redux 스토어
+// import store from '@redux/store';
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </StrictMode>
+// );
+
+// Redux toolkit 스토어
+import store from '@/RTK/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
