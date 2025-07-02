9729ad3dbe6de1038ad64f930ecb5b2f772d299d
seonyoungg <tjsduddlfma1@naver.com>
2025-07-02 11:53:18 +0900

    NextResponse수정

--- workspace/ch10-nextjs/02-cna/src/app/api/posts/[id]/route.ts
+++ workspace/ch10-nextjs/02-cna/src/app/api/posts/[id]/route.ts
@@ -1,29 +1,25 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   console.log('GET 라우트 핸들러', id);
-
-  // DB 연동해서 상세 정보 조회 작업을 직접 구현(풀스택)
-  // 준비된 API 서버 호출
+  // db연동해서 상세 정보 조회 작업을 직접 구현(풀스택)
+  // 준비된 api서버 호출
   const res = await fetch(`https://fesp-api.koyeb.app/market/posts/${id}`, {
     headers: {
-      'Client-id': 'openmarket',
+      'Client-Id': 'openmarket',
     },
   });
 
   const data = await res.json();
-  // const data = {
-  //   id,
-  //   title: '가짜 제목',
-  // };
-  return Response.json(data);
+  //Response는 fetch함수에서 기본으로 제공되는 내장함수
+  return NextResponse.json(data);
 }
 
 export function POST() {
-  return Response.json('route handler의 POST 응답');
+  return NextResponse.json('route handler의 POST 응답');
 }
 
 export function DELETE() {
-  return Response.json('route handler의 DELETE 응답');
+  return NextResponse.json('route handler의 DELETE 응답');
 }
