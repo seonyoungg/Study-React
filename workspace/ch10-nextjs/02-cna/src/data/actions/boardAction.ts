2ab7afdadc20cd5e72269647dc74a8713a68a47e
seonyoungg <tjsduddlfma1@naver.com>
2025-07-03 17:03:01 +0900

    캐시관련

--- workspace/ch10-nextjs/02-cna/src/data/actions/boardAction.ts
+++ workspace/ch10-nextjs/02-cna/src/data/actions/boardAction.ts
@@ -1,28 +1,45 @@
 'use server';
 
 import { PostInfoRes } from '@/types/board';
+// import { revalidatePath } from 'next/cache';
+import { revalidateTag } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 // 이 컴포넌트는 서버에서만 실행되어야 하며,
 // 클라이언트 측에서도 원격 호출 방식(RPC)으로 사용할 수 있도록 설정된 함수
 
-// 게시글 등록
+// 게시글 등록 함수
+// prevState: 이전 상태(예: useFormState 사용 시 전달), formData: 사용자가 입력한 폼 데이터
 export async function createPost(prevState: PostInfoRes, formData: FormData) {
   const title = formData.get('title');
   const content = formData.get('content');
-  // 많은 속성을 지닌 데이터를 일괄로 가져올 때
+
+  // ※ 참고: 모든 formData 값을 한꺼번에 가져올 수 있음 (여러 필드가 많을 경우 유용)
+  // cho9-ajax/p1-board-fetch/src/pages/board/commentNew 참고하기
   // const jsonBody = Object.fromEntries(formData.entries());
 
+  // 게시글 등록 API 요청
   const res = await fetch(`https://fesp-api.koyeb.app/market/posts`, {
     method: 'POST',
-    body: JSON.stringify({ title, content, type: 'qna' }),
+    body: JSON.stringify({ title, content, type: 'qna' }), // 전송할 JSON 데이터 (Q&A 타입으로 고정)
     headers: {
-      'Client-Id': 'openmarket',
-      'Content-Type': 'application/json',
+      'Client-Id': 'openmarket', // API 인증용 클라이언트 ID
+      'Content-Type': 'application/json', // 요청 본문이 JSON 형식임을 명시
     },
   });
   const data = await res.json();
 
+  // 성공적으로 등록되었을 경우
+  if (data.ok) {
+    // '/posts' 페이지에 대한 캐시를 무효화 (최신 게시글 목록 반영을 위해)
+    // revalidatePath('/posts'); // => revalidate 필요 없음
+
+    // 'list'라는 태그를 가진 데이터의 캐시를 무효화
+    // 해당 태그를 사용하는 페이지(예: 게시글 목록)가 최신 데이터를 반영하도록 함
+    revalidateTag('list');
+
+    // '/posts' 페이지로 이동
+    redirect('/posts');
+  }
   return data;
 }
-// cho9-ajax/p1-board-fetch/src/pages/board/commentNew 참고하기
-// 한번에 데이터 꺼내오는거 있음
--- workspace/ch10-nextjs/02-cna/src/data/functions/boardFetch.ts
+++ workspace/ch10-nextjs/02-cna/src/data/functions/boardFetch.ts
@@ -2,12 +2,28 @@
 
 import { Post } from '@/types/board';
 
+// 게시글 조회 함수
 export async function fetchPosts(): Promise<Post[]> {
-  // 준비된 api서버 호출
+  // API 서버에 GET 요청을 보냄
   const res = await fetch(`https://fesp-api.koyeb.app/market/posts?type=qna`, {
+    // API 요청에 필요한 클라이언트 식별자 지정
     headers: {
       'Client-Id': 'openmarket',
     },
+    next: {
+      tags: ['list', 'qna'], // 태그 중복 처리 또한 가능
+      // 이 fetch 요청의 응답을 'list' 태그로 캐시 지정(revalidateTag('list')로 무효화 가능)
+      revalidate: 10, // 10초 후에 자동으로 revalidate
+    },
+    // cache : 'no-cache'
+    cache: 'force-cache',
+    /*
+    - Next.js 15 이후 기본값 cache : 'no-cache' 
+      매번 서버에 요청해서 최신 데이터를 받는 방식
+      'reload'는 항상 네트워크에서 데이터를 가져오며, 캐시는 무시함
+    - Next.js 14의 기본값 cache : 'force-cache' 
+      캐시된 응답이 있으면 그것을 사용하고,없으면 요청 후 캐시에 저장 (정적 데이터에 적합)
+     */
   });
 
   const data = await res.json();
